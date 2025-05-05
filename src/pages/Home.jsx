c59ed63d888d00c4ebc594471ec4ac43389edaa6
lalith kumar <lalithdev123@gmail.com>
2025-05-05 16:36:54 +0530

    initial commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Combos from "../components/HomePage/Combos";
+import Portfolio from "../components/HomePage/portofolio";
+import Testimonials from "../components/HomePage/Testimonials";
+import OurServices from "../components/HomePage/OurServices";
+import Hero from "../components/HomePage/Hero";
+import WhyChooseUs from "../components/HomePage/ WhyChooseUs";
+
+
+const Home=()=>{
+  return (
+    <div className="bg-black  text-white  min-h-screen">
+      <Hero/>
+      <Combos/>
+      <OurServices/>
+      <Portfolio/>
+      <WhyChooseUs/>
+      <Testimonials/>
+    </div>
+  );
+};
+
+export default Home;
